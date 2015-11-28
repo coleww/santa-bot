d705a5016d18e63aebdb2af9349fc0ea65fde384
Cole Willsea <coleww@gmail.com>
2015-11-28 09:58:22 -0800

    crucial, yeah

--- watcher.js
+++ watcher.js
@@ -6,5 +6,6 @@ var redis = require('redis')
 var client = redis.createClient()
 
 stream.on('follow', function (t) {
+  console.log(t)
   client.rpush('santafy', JSON.stringify({target: t.source.screen_name, id_str: t.source.id_str}), redis.print)
 })
