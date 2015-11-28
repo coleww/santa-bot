5eedf9cfe32aebf836c9eead024ca2f3674597d8
Cole Willsea <coleww@gmail.com>
2015-11-28 10:14:01 -0800

    idk

--- tweeter.js
+++ tweeter.js
@@ -13,6 +13,7 @@ var replyInterval = config.replyInterval
 
 function naughtyOrNice(un, cb){
   reqclient.get('@' + un, function(err, targ) {
+    console.log(err, targ)
     var the_tweets = JSON.parse(targ).data.map(function (l) {return l.text})
     var total = the_tweets.map(function (t) {
       return sentiment(t)
@@ -33,52 +34,31 @@ client.lpop('santafy', function (err, user) {
   } else if (user !== null) {
     var t = JSON.parse(user)
     // if not null, check if we've already tweeted at this user recently. no thirsty randos plz
-    client.exists('&SANTA' + t.target, function (err, exists) {
-      console.log(t.target, 'we have replied to this user', exists)
-      var timestamp = new Date().getTime()
-      // if we haven't already tweeted, tweet and save a timestamp
-      if (err) {
-        console.log(err)
-        client.end()
-        throw "a party"
-      } else{
-        client.get('&SANTA' + t.target, function (err, lastTweeted) {
-          if (err) {
-            console.log(err)
+    naughtyOrNice(t.target, function(d){
+      var listName = d >= 0 ? 'nice' : 'naughty'
+      if (config.live) {
+        T.post('friendships/create', {screen_name: t.target}, function (e, d, r){
+          if (e) {
+            console.log(t.id_str, 'replyerr:', e)
+            // close connection and program
             client.end()
             throw "a party"
-          } else{
-            if (!exists) {
-              naughtyOrNice(t.target, function(d){
-                var listName = d >= 0 ? 'nice' : 'naughty'
-                if (config.live) {
-                  T.post('friendships/create', {screen_name: t.target}, function (e, d, r){
-                    if (e) {
-                      console.log(t.id_str, 'replyerr:', e)
-                      // close connection and program
-                      client.end()
-                      throw "a party"
-                    } else {
-                      T.post('lists/members/create', {slug: listName, owner_screen_name: 'santaBot5000', user_id: t.id_str}, function (err, data, response) {
-                        if (err) {
-                          console.log(t.id_str, 'replyerr:', err)
-                          // close connection and program
-                          client.end()
-                          throw "a party"
-                        } else {
-                          console.log(t.id_str, 'reply:', data)
-                          // record current timestamp for this user
-                          client.set('&SANTA' + t.target, timestamp + '', function () {
-                            client.end()
-                            throw "a party"
-                          })
-                        }
-                      })
-                    }
-                  })
-                }
-              })
-            }
+          } else {
+            T.post('lists/members/create', {slug: listName, owner_screen_name: 'santaBot5000', user_id: t.id_str}, function (err, data, response) {
+              if (err) {
+                console.log(t.id_str, 'replyerr:', err)
+                // close connection and program
+                client.end()
+                throw "a party"
+              } else {
+                console.log(t.id_str, 'reply:', data)
+                // record current timestamp for this user
+                client.set('&SANTA' + t.target, timestamp + '', function () {
+                  client.end()
+                  throw "a party"
+                })
+              }
+            })
           }
         })
       }
