214c1d9c78019378aa341c90c12fe907cada96f0
Cole Willsea <coleww@gmail.com>
2015-12-13 08:25:44 -0800

    Create checkLists.js

---
+++ checkLists.js
@@ -0,0 +1,21 @@
+var Twit = require('twit')
+var config = require('./config')
+
+var T = new Twit(config.twitter)
+
+T.get('lists/members', {owner_screen_name: 'santaBot5000', slug: 'nice'}, function (err, list, res){
+  check(list)
+  T.get('lists/members', {owner_screen_name: 'santaBot5000', slug: 'naughty'}, function (err, list, res){
+    check(list)
+    T.get('lists/members', {owner_screen_name: 'santaBot5000', slug: 'nice'}, function (err, list, res){
+      check(list)
+      T.get('lists/members', {owner_screen_name: 'santaBot5000', slug: 'naughty'}, function (err, list, res){
+        check(list)
+      })
+    })
+  })
+})
+ 
+function check (list) {
+  console.log(list) 
+}
